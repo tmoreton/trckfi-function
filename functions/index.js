6fdfd66608ac0815fdf6e90c04e5b1d48849a2ca
Tim Moreton <tmoreton@inspirecleanenergy.com>
2022-10-17 15:51:42 -0400

    working on merging transactions and recurring

--- functions/index.js
+++ functions/index.js
@@ -10,11 +10,36 @@ exports.email_csv = functions.https.onRequest(async function(req, res) {
   const email = req.query.email;
   functions.logger.log("UID:", uid);
   functions.logger.log("Email:", email);
-  admin.firestore().collection("users").doc(uid).collection("recurring").get().then(snapshot => {
+  admin.firestore().collection("users").doc(uid).collection("transactions").get().then(transactions => {
     var recurring = [];
-    snapshot.forEach(doc => {
-      recurring = recurring.concat(doc.data());
+    functions.logger.log("Transactions Data:", transactions);
+    transactions.forEach(transaction => {
+      let data = transaction.data()
+      recurring = recurring.concat({
+        'Place': data.place.main_text,
+        'Amount': data.amount,
+        'Category': data.icon.title,
+        'Tags': JSON.stringify(data.chips),
+        'Type': data.type,
+        'Recurring': data.recurring,
+        'Transaction Date': new Date(data.created_dt)
+      });
     });
+    admin.firestore().collection("users").doc(uid).collection("recurring").get().then(snapshot => {
+      functions.logger.log("Recurring Data:", snapshot);
+      snapshot.forEach(doc => {
+        let recurring_data = doc.data()
+        recurring = recurring.concat({
+          'Place': recurring_data.place.main_text,
+          'Amount': recurring_data.amount,
+          'Category': recurring_data.icon.title,
+          'Tags': JSON.stringify(recurring_data.chips),
+          'Type': recurring_data.type,
+          'Recurring': recurring_data.recurring,
+          'Transaction Date': recurring_data.created_dt
+        });
+      });
+    })
     return json2csv(recurring);
   }).then((csv) => {
     const mailTransport = nodemailer.createTransport({
@@ -29,21 +54,15 @@ exports.email_csv = functions.https.onRequest(async function(req, res) {
     const mailOptions = {
       from: '"Trckfi" <tim@trckfi.com>',
       to: email,
-      subject: "Your Trckfi Data Is Ready For Download",
+      subject: "Your Trckfi data is ready for download",
       attachments: [{   
-        filename: 'report.csv',
+        filename: 'trckfi-report.csv',
         content: csv 
       }]
     };
     mailTransport.sendMail(mailOptions);
     res.status(200);
-
-    res.setHeader(
-      "Content-disposition",
-      "attachment; filename=report.csv"
-    );
-    res.set("Content-Type", "text/csv");
-    res.send(csv);
+    res.send("Success");
   }).catch((error) => {
     console.error(error);
   });
