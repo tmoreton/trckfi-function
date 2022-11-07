fa63325e3b9e28245a164f8c4ab7d58999037e9f
Tim Moreton <tmoreton@inspirecleanenergy.com>
2022-11-07 09:09:56 -0500

    add new endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,12 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const json2csv = require("json2csv").parse;
+const nodemailer = require("nodemailer");
 const { Configuration, PlaidApi, PlaidEnvironments } = require("plaid");
 admin.initializeApp();
 
-const json2csv = require("json2csv").parse;
-const nodemailer = require("nodemailer");
 const configuration = new Configuration({
-  basePath: PlaidEnvironments.sandbox,
+  basePath: PlaidEnvironments.development,
   baseOptions: {
     headers: {
       'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
@@ -16,32 +16,33 @@ const configuration = new Configuration({
 });
 
 const client = new PlaidApi(configuration);
+const app = require('express')();
 
-exports.create_token = functions.https.onRequest(async function(req, res) {
-  functions.logger.log("Starting plaid stuff");
-  try{
-    const request = {
-      user: {
-        client_user_id: 'user-id',
-      },
-      client_name: 'Plaid Test App',
-      products: ['transactions'],
-      language: 'en',
-      webhook: 'https://webhook.example.com',
-      redirect_uri: 'https://domainname.com/oauth-page.html',
-      country_codes: ['US'],
-    };
+// We'll enable CORS support to allow the function to be invoked from our app client-side.
+app.use(require('cors')({origin: true}));
 
-    const createTokenResponse = await client.post(request);
-    // res.json(createTokenResponse.data);
-    functions.logger.log("token res:", createTokenResponse);
-    res.status(200);
-    res.send(createTokenResponse.data);
-  } catch (error) {
-    functions.logger.log("token error:", error);
-    res.send(error);
-  }
+const params = {
+  user: {
+    // This should correspond to a unique id for the current user.
+    client_user_id: 'clientUserId',
+  },
+  client_id: process.env.PLAID_CLIENT_ID,
+  secret: process.env.PLAID_SECRET,
+  client_name: 'Trckfi',
+  products: ['auth', 'transactions'],
+  language: 'en',
+  country_codes: ['US']
+};
+
+// Add a route handler to the app to generate the secured key
+app.post('https://development.plaid.com/link/token/create', params, (req, res) => {
+  functions.logger.log("req:", req);
+  functions.logger.log("res:", res);
+  res.status(200);
 });
+
+// Finally, pass our ExpressJS app to Cloud Functions as a function
+exports.create_plaid_token = functions.https.onRequest(app);
 
 exports.email_csv = functions.https.onRequest(async function(req, res) {
   const uid = req.query.uid;
@@ -86,6 +87,39 @@ exports.email_csv = functions.https.onRequest(async function(req, res) {
   functions.logger.log("Recurring Data:", recurring_data);
 
   let csv = json2csv(transaction_data.concat(recurring_data))
+  const mailTransport = nodemailer.createTransport({
+    host: "mail.privateemail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "tim@trckfi.com",
+      pass: process.env.EMAIL_PASSWORD,
+    }
+  });
+
+  const mailOptions = {
+    from: '"Trckfi" <tim@trckfi.com>',
+    to: email,
+    subject: "Your Trckfi data is ready for download",
+    attachments: [{   
+      filename: 'trckfi-report.csv',
+      content: csv 
+    }]
+  };
+  mailTransport.sendMail(mailOptions);
+  res.status(200);
+  res.send("Success");
+});
+
+
+exports.send_email = functions.https.onRequest(async function(req, res) {
+  const uid = req.query.uid;
+  const email = req.query.email;
+  const data = req.query.data;
+  functions.logger.log("UID:", uid);
+  functions.logger.log("Email:", email);
+
+  let csv = json2csv(data)
   const mailTransport = nodemailer.createTransport({
     host: "mail.privateemail.com",
     port: 465,
